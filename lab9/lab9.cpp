a93bc0837d5680dc25f6457c3c35b3a0f30141e8
Алексей Бабашев <alex.babashev@gmail.com>
2021-12-27 18:01:37 +0200

    lab9

--- lab9/lab9.cpp
+++ lab9/lab9.cpp
@@ -1,13 +1,88 @@
+
 #include <iostream>
-#include<ctime>
-#include<cmath>
-#include<iomanip>
 #include <string>
 
 using namespace std;
 
+string enterString();
+string* divideWords(string, int&, int&);
+int Sum(string* , int);
+bool WrongSymbol(string);
+
+int main()
+{
+    int index = 0;
+
+    string Entered;
+    string* dividedWords;
+
+    do
+    {
+        Entered = enterString();
+    } while (WrongSymbol(Entered));
+
+    int n = count(Entered.begin(), Entered.end(), ' ') + 1;
+
+    dividedWords = divideWords(Entered, n, index);
+
+    int sum = Sum(dividedWords, n);
 
 
-int main() {
+    cout << "sum = " << sum;
+}
 
+string enterString()
+{
+    string result;
+
+    cout << "enter string"<<endl;
+    getline(cin, result);
+
+    return result;
+}
+
+string* divideWords(string text, int& n, int& index)
+{
+    
+
+    string* arrayOfWords = new string[n];
+    string currWord = "";
+
+    for (int i = 0; i <= text.size(); i++)
+    {
+        if (text[i] != ' ' && i < text.size())
+            currWord += text[i];
+        else
+        {
+            arrayOfWords[index] = currWord;
+            currWord = "";
+            index++;
+        }
+
+    }
+    return arrayOfWords;
+}
+
+int Sum(string* words, int n)
+{
+
+    int res = 0, valid_position = 0;
+    for (int i = 0; i < n; i++) {
+        res += stoi(words[i]);
+    }
+
+    return res;
+}
+
+bool WrongSymbol(string entered)
+{
+    bool res = false;
+    for (int i = 0; i < entered.length(); i++)
+        if (entered[i] != ' ' && (entered[i] < '0' || entered[i] > '9'))
+        {
+            res = true;
+        }
+    if (res)
+        cout << "incorrect symbol. try againe"<<endl;
+    return res;
 }
